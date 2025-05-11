2fa7b05f7646d20b631b4db08bf968301474d667
longZam <longZam305@gmail.com>
2025-05-11 05:54:06 +0000

    Convert BVH from pointer-based to array-based

--- src/DataStructure/BVH.cs
+++ src/DataStructure/BVH.cs
@@ -1,230 +1,236 @@
+using System.Diagnostics;
 using System.Numerics;
-using System.Runtime.InteropServices;
 
 namespace DataStructure;
 
 
-public readonly struct Bounds(Vector3 center, float radius)
+public readonly struct Bounds(Vector3 center, Vector3 size)
 {
     public readonly Vector3 Center = center;
-    public readonly float Radius = radius;
-    public Vector3 Min => Center - new Vector3(Radius, Radius, Radius);
-    public Vector3 Max => Center + new Vector3(Radius, Radius, Radius);
+    public readonly Vector3 Size = size;
+    public Vector3 Extends => Size * 0.5f;
+    public Vector3 Min => Center - Extends;
+    public Vector3 Max => Center + Extends;
 
 
     public static Bounds Union(Bounds a, Bounds b)
     {
-        var dv = b.Center - a.Center;
-        var d = dv.Length();
-        
-        // 한 원이 다른 원을 완전히 포함하면
-        if (MathF.Abs(a.Radius - b.Radius) > d)
-            return a.Radius > b.Radius ? a : b;
+        var min = Vector3.Min(a.Min, b.Min);
+        var max = Vector3.Max(a.Max, b.Max);
+        var center = (max + min) * 0.5f;
+        var size = max - min;
 
-        var r = (d + a.Radius + b.Radius) / 2;
-        var ratio = (r - a.Radius) / d;
-        var c = new Vector3(
-            a.Center.X + dv.X * ratio,
-            a.Center.Y + dv.Y * ratio,
-            a.Center.Z + dv.Z * ratio
-        );
-
-        return new(c, r);
+        return new Bounds(center, size);
     }
 
     public bool Contains(Bounds other)
     {
-        return Radius - other.Radius >= Vector3.Distance(Center, other.Center);
+        return Min.X <= other.Min.X && other.Max.X <= Max.X &&
+                Min.Y <= other.Min.Y && other.Max.Y <= Max.Y &&
+                Min.Z <= other.Min.Z && other.Max.Z <= Max.Z;
+    }
+
+    public bool Overlaps(Bounds other)
+    {
+        return !(Max.X < other.Min.X || Min.X > other.Max.X ||
+                 Max.Y < other.Min.Y || Min.Y > other.Max.Y ||
+                 Max.Z < other.Min.Z || Min.Z > other.Max.Z);
+    }
+
+    public float SurfaceArea()
+    {
+        return Size.X * Size.Y + Size.X * Size.Z + Size.Y * Size.Z * 2;
     }
 }
 
-public sealed class BVH<T>
-    where T : struct
+public sealed class BVH<T> where T : struct
 {
-    private class Node(T item = default)
+    private struct Node
     {
-        public readonly T Item = item;
+        public enum Type
+        {
+            None,
+            Leaf,
+            Interior
+        }
+
         public Bounds bounds;
-        public Node? left, right, parent;
+        public T item;
+        public Type type;
+        public int height;
     }
 
+    private readonly Dictionary<T, int> leafNodes;
+    private Node[] nodes;
 
-    private readonly Dictionary<T, Node> leafNodeMap;
-    private Node? root;
+    public int Capacity { get; private set; }
+    public int Count { get; private set; }
 
 
-    public BVH()
+    public BVH(int capacity)
     {
-        this.root = null;
-        this.leafNodeMap = new Dictionary<T, Node>();
+        this.leafNodes = new Dictionary<T, int>(capacity);
+        this.nodes = new Node[capacity];
+        this.Capacity = capacity;
+        this.Count = 0;
     }
 
-    public void Traversal(Func<Bounds, bool> predicate, Action<T> callback)
+    public void Insert(T item, Bounds bounds)
     {
-        if (root == null)
-            return;
-        
-        DFS(predicate, callback, root);
-    }
+        Debug.Assert(!leafNodes.ContainsKey(item));
+        Count += 1;
+        leafNodes.Add(item, 0);
 
-    private static void DFS(Func<Bounds, bool> predicate, Action<T> callback, Node current)
-    {
-        if (!predicate(current.bounds))
-            return;
-
-        if (current.left == null || current.right == null)
-            callback(current.Item);
-        else
+        if (nodes[0].type == Node.Type.None)
         {
-            DFS(predicate, callback, current.right);
-            DFS(predicate, callback, current.left);
+            nodes[0] = new Node
+            {
+                bounds = bounds,
+                item = item,
+                type = Node.Type.Leaf
+            };
+            leafNodes[item] = 0;
+            return;
         }
-    }
 
-    public void Add(T item, Bounds bounds)
-    {
-        var newLeaf = new Node(item)
+        var current = 0;
+
+        while (nodes[current].type == Node.Type.Interior)
         {
-            bounds = bounds
+            // 표면적 휴리스틱
+            var lb = Bounds.Union(bounds, nodes[current * 2 + 1].bounds).SurfaceArea() * nodes[current * 2 + 1].height * 5;
+            var rb = Bounds.Union(bounds, nodes[current * 2 + 2].bounds).SurfaceArea() * nodes[current * 2 + 2].height * 5;
+
+            current = (current * 2) + (lb < rb ? 1 : 2);
+        }
+
+        if (Capacity <= current * 2 + 2)
+        {
+            Capacity *= 2;
+            Array.Resize(ref nodes, Capacity);
+        }
+
+        nodes[current * 2 + 1] = nodes[current];
+        nodes[current * 2 + 2] = new Node
+        {
+            bounds = bounds,
+            item = item,
+            type = Node.Type.Leaf
         };
+        nodes[current].type = Node.Type.Interior;
+        Refit(current);
 
-        leafNodeMap.Add(item, newLeaf);
-        Insert(newLeaf);
-    }
-
-    private void Insert(Node node)
-    {
-        if (root == null)
-        {
-            root = node;
-            return;
-        }
-        
-        var current = root;
-
-        while (current.left != null && current.right != null)
-        {
-            // SAH 표면적 휴리스틱
-            // 구의 표면적은 반지름과 비례함을 이용
-
-            var lb = Bounds.Union(node.bounds, current.left.bounds);
-            var rb = Bounds.Union(node.bounds, current.right.bounds);
-            current = lb.Radius < rb.Radius ? current.left : current.right;    
-        }
-
-        // 현재 노드는 leaf node임
-        if (current == root)
-        {
-            root = new Node();
-            root.left = current;
-            root.right = node;
-            current.parent = root;
-            node.parent = root;
-
-            RefitBounds(root);
-        }
-        else
-        {
-            // parent는 반드시 internal임. root가 아닌데 parent가 없으면 오류임
-            var parent = current.parent ?? throw new Exception();
-            var left = parent.left == current;
-            var newInternal = new Node();
-
-            if (left)
-                parent.left = newInternal;
-            else
-                parent.right = newInternal;
-            
-            newInternal.left = left ? current : node;
-            newInternal.right = left ? node : current;
-            newInternal.parent = parent;
-            current.parent = newInternal;
-            node.parent = newInternal;
-
-            RefitBounds(newInternal);
-        }
+        leafNodes[nodes[current * 2 + 1].item] = current * 2 + 1;
+        leafNodes[item] = current * 2 + 2;
     }
 
     public bool Remove(T item)
     {
-        if (!leafNodeMap.Remove(item, out var node))
+        if (!leafNodes.Remove(item, out var index))
             return false;
-        
-        if (node == root)
-        {
-            root = null;
-            return true;
-        }
 
-        var parent = node.parent ?? throw new Exception();
-        var left = parent.left == node;
-        var other = (left ? parent.right : parent.left) ?? throw new Exception();
-        other.parent = parent.parent;
+        nodes[index].type = Node.Type.None;
 
-        // root
-        if (parent.parent == null)
-            root = other;
-        else
-        {
-            if (parent.parent.left == parent)
-                parent.parent.left = other;
-            else
-                parent.parent.right = other;
-
-            RefitBounds(parent.parent);
-        }
+        if (index != 0)
+            RebuildSubtree((index - 1) / 2);
 
         return true;
     }
 
-    public void Move(T item, Bounds newBounds)
+    private readonly System.Collections.Generic.Queue<Node> rebuildQueue = new System.Collections.Generic.Queue<Node>(8192);
+    private readonly System.Collections.Generic.Stack<int> rebuildStack = new System.Collections.Generic.Stack<int>(8192);
+
+    private void RebuildSubtree(int subtree)
     {
-        var leaf = leafNodeMap[item];
-        leaf.bounds = newBounds;
-        var parent = leaf.parent ?? throw new Exception();
+        // 서브 트리 내 모든 leaf 노드 수집
+        GetLeafsFromSubtree(subtree);
 
-        if (parent.bounds.Contains(leaf.bounds))
-            return;
-        
-        RefitBounds(parent);
-    }
+        // 서브 트리 루트만 남기고 모두 제거
+        RemoveSubtree(subtree);
 
-    // 노드 최적화
-    public void Optimize()
-    {
-        if (root == null)
-            return;
-        
-        RecursiveUnlink(root);
-        root = null;
-
-        foreach (var node in leafNodeMap.Values)
-            Insert(node);
-    }
-
-    private static void RecursiveUnlink(Node current)
-    {
-        current.parent = null;
-
-        if (current.left == null || current.right == null)
-            return;
-
-        RecursiveUnlink(current.left);
-        RecursiveUnlink(current.right);
-
-        current.left = null;
-        current.right = null;
-    }
-
-    private static void RefitBounds(Node start)
-    {
-        Node? current = start;
-
-        while (current?.left != null && current?.right != null)
+        while (rebuildQueue.TryDequeue(out var node))
         {
-            var bounds = Bounds.Union(current.left.bounds, current.right.bounds);
-            current.bounds = new Bounds(bounds.Center, bounds.Radius * 1.1f);
-            current = current.parent;
+            var current = subtree;
+
+            if (nodes[current].type == Node.Type.None)
+            {
+                nodes[current] = node;
+                leafNodes[node.item] = current;
+                continue;
+            }
+
+            while (nodes[current].type == Node.Type.Interior)
+            {
+                // 표면적 휴리스틱
+                var lb = Bounds.Union(node.bounds, nodes[current * 2 + 1].bounds).SurfaceArea() * nodes[current * 2 + 1].height * 5;
+                var rb = Bounds.Union(node.bounds, nodes[current * 2 + 2].bounds).SurfaceArea() * nodes[current * 2 + 2].height * 5;
+
+                current = (current * 2) + (lb < rb ? 1 : 2);
+            }
+
+            nodes[current * 2 + 1] = nodes[current];
+            nodes[current * 2 + 2] = node;
+            nodes[current].type = Node.Type.Interior;
+            Refit(current);
+
+            leafNodes[nodes[current * 2 + 1].item] = current * 2 + 1;
+            leafNodes[node.item] = current * 2 + 2;
+        }
+    }
+
+    private void GetLeafsFromSubtree(int subtree)
+    {
+        if (nodes[subtree].type == Node.Type.Interior)
+        {
+            GetLeafsFromSubtree(subtree * 2 + 1);
+            GetLeafsFromSubtree(subtree * 2 + 2);
+        }
+        else if (nodes[subtree].type == Node.Type.Leaf)
+        {
+            rebuildQueue.Enqueue(nodes[subtree]);
+        }
+    }
+
+    private void RemoveSubtree(int subtree)
+    {
+        if (nodes[subtree].type == Node.Type.Interior)
+        {
+            RemoveSubtree(subtree * 2 + 1);
+            RemoveSubtree(subtree * 2 + 2);
+        }
+
+        nodes[subtree].type = Node.Type.None;
+        nodes[subtree].height = 0;
+    }
+
+    public void Traversal(Func<Bounds, bool> predicate, Action<T> callback, int current = 0)
+    {
+        if (nodes[current].type == Node.Type.None)
+            return;
+
+        if (!predicate(nodes[current].bounds))
+            return;
+
+        if (nodes[current].type == Node.Type.Interior)
+        {
+            Traversal(predicate, callback, current * 2 + 1);
+            Traversal(predicate, callback, current * 2 + 2);
+        }
+        else
+        {
+            callback(nodes[current].item);
+        }
+    }
+
+    private void Refit(int current)
+    {
+        current = current * 2 + 1;
+
+        while (current != 0)
+        {
+            current = (current - 1) / 2;
+            nodes[current].bounds = Bounds.Union(nodes[current * 2 + 1].bounds, nodes[current * 2 + 2].bounds);
+            nodes[current].height = Math.Max(nodes[current * 2 + 1].height, nodes[current * 2 + 2].height) + 1;
         }
     }
 }
